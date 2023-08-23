498ee232a9c8c1675b52996a0550b3d75fa9940a
Jonas Holbech <jh@jonasholbech.dk>
2023-08-23 12:19:03 +0200

    updat km

--- public/snippets/t7/6.templates-and-arrays/arrays-intro.js
+++ public/snippets/t7/6.templates-and-arrays/arrays-intro.js
@@ -4,4 +4,4 @@ const placesVisitedIn2020 = [];
 const placesVisitedIn2021 = [];
 const placesVisitedIn2022 = ["Austria", "Czech Republic"];
 
-const kmToWorkThisWeek = [0, 11, 0, 0, 11];
+const kmToWorkThisWeek = [11, 11, 0, 11, 11];
