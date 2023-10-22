44dc6a9eee79b2cc5843909371c264be76693cb8
Jonas Holbech <jh@jonasholbech.dk>
2023-10-22 17:09:46 +0200

    5.npm

--- public/snippets/react/0.need-to-know-js/ex/mapping.js
+++ public/snippets/react/0.need-to-know-js/ex/mapping.js
@@ -2,7 +2,7 @@ const searchengines = [
   { target: "https://google.com", name: "Google" },
   { target: "https://bing.com", name: "Bing" },
 ];
-let links = "";
+let links = [];
 searchengines.forEach((link) => {
   links.push(`<a href="${link.target}">${link.name}</a>`);
 });
